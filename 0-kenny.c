873f9ea460f11e6d881c11616a9f4a03f7f17c80
Arowolokehinde <arowolokehinde321@gmail.com>
2023-10-16 09:32:47 -0700

    my_printf

--- 0-kenny.c
+++ 0-kenny.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 	int amount = 0;
 	va_list num_arg;
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(num_arg, format);
 	while (*format)
